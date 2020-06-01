b4eb2ec7b895b1c912611f432c3a0c32e5312274
Raylandson <62724100+Raylandson@users.noreply.github.com>
2020-06-01 13:23:15 -0300

    update.096.py

--- ex096.py
+++ ex096.py
@@ -1,20 +1,15 @@
-def showline(x):
-    print('-' * x)
+from urllib.request import urlretrieve
+from os import remove, stat
 
+x, y = map(int, input('what is the chapter range? ').split())
+for a in range(x - 1, y):
+    c = 1
+    while True:
+        urlretrieve(f'https://mangassuki.xyz/sys_mangas/00sdfrw/O/one-piece/cap-{a+1}/{c:04}.jpg', f'cap-{a+1}_{c}.jpg')
+        # https://mangassuki.xyz/sys_mangas/00sdfrw/O/one-piece/cap-1/0001.jpg
+        print(f'https://mangassuki.xyz/sys_mangas/00sdfrw/O/one-piece/cap-{a+1}/{c:04}.jpg -- cap-{a+1}_{c}.jpg')
+        if stat(f'/home/raylandson/PycharmProjects/atividadesCv/cap-{a+1}_{c}.jpg').st_size < 12000:
+            remove(f'/home/raylandson/PycharmProjects/atividadesCv/cap-{a+1}_{c}.jpg')
+            break
+        c += 1
 
-list = [showline(5), print('123')]
-showline(5)
-
-print(list[0])
-l1 = [1, 2, 3]
-l2 = [2, 5, 7]
-
-
-def arraymult(x, y):
-    for a in x:
-        print()
-        for b in y:
-            print(a * b, end='; ')
-
-
-arraymult(l1, l2)
