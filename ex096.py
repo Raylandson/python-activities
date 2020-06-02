c1e54d6ba4b94791b581ad2828df6699ed578b6e
Raylandson <62724100+Raylandson@users.noreply.github.com>
2020-06-02 15:45:11 -0300

    update ex096.py
    now i check the site to know the page status.

--- ex096.py
+++ ex096.py
@@ -1,15 +1,27 @@
-from urllib.request import urlretrieve
-from os import remove, stat
+from urllib.request import *
+import requests
 
-x, y = map(int, input('what is the chapter range? ').split())
+x, y = map(int, input('what is the chapters range?ex 1 4:  ').split())
 for a in range(x - 1, y):
     c = 1
     while True:
-        urlretrieve(f'https://mangassuki.xyz/sys_mangas/00sdfrw/O/one-piece/cap-{a+1}/{c:04}.jpg', f'cap-{a+1}_{c}.jpg')
-        # https://mangassuki.xyz/sys_mangas/00sdfrw/O/one-piece/cap-1/0001.jpg
-        print(f'https://mangassuki.xyz/sys_mangas/00sdfrw/O/one-piece/cap-{a+1}/{c:04}.jpg -- cap-{a+1}_{c}.jpg')
-        if stat(f'/home/raylandson/PycharmProjects/atividadesCv/cap-{a+1}_{c}.jpg').st_size < 12000:
-            remove(f'/home/raylandson/PycharmProjects/atividadesCv/cap-{a+1}_{c}.jpg')
-            break
+        url = f'https://unionmangas.top/leitor/mangas/One%20Piece/{a + 1:02}/{c:03}.png'
+        page = requests.get(url.replace("\r", "").replace("\n", ""))
+        print(f'downloading cap-{a+ 1}_{c}')
+        print(page.status_code)
+        if page.status_code != 200:
+            url = f'https://unionmangas.top/leitor/mangas/One%20Piece/{a + 1:02}/{c:03}.jpg'
+            page = requests.get(url.replace("\r", "").replace("\n", ""))
+            print(page.status_code)
+            if page.status_code != 200:
+                print('Stopping')
+                break
+            opener = URLopener()
+            opener.addheader('User-Agent', 'whatever')
+            filename, headers = opener.retrieve(url, f'cap-{a + 1}_{c-1}.jpg')
+        else:
+            opener = URLopener()
+            opener.addheader('User-Agent', 'whatever')
+            filename, headers = opener.retrieve(url, f'cap-{a + 1}_{c}.png')
         c += 1
 
